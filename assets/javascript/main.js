c2cc83d2a53e1b99a105c532be16a108eee972d7
kobbootcamp <kobbootcamp@gmail.com>
2018-11-03 12:41:11 -0500

    kevin made a goof

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,38 @@
+// function jupload()
+// {
+//     $(".imageholder").append('<img src="./images/loading.gif">');
+// }
+
+function juploadstop(result)
+{
+    if(result==0)
+    {
+        $(".imageholder").html("");
+
+    }
+    // the result will be the path to the image
+    else if(result!=0)
+    {
+        $(".imageholder").html("");
+        // imageplace is the class of the div where you want to add the image  
+        $(".imageplace").append("<img src='"+result+"'>");
+    }   
+}
+
+$('#submit_form').on("click", function(){
+    $.ajax({
+      type: 'POST',
+      url: path/fileUpload.php,
+      data: image_input_name
+    });
+  
+  //after submitting, get the url of the image form the server
+  
+    $('#div_to_display_image').html("<img src='path/image_file.jpg' alt='this' />");
+  
+  });
+
+
 // function lastResult (heroName) {
 
 // $('.list-group').append("<li class='list-group-item'>"+"You could be related to " + heroname +" </li>")
