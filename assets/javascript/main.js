64e18b84a0771422f8db10f561a82675dca6f2d8
Lamuel27 <samrelliner@gmail.com>
2018-11-07 20:11:07 -0600

    faceplus

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,44 +1,60 @@
+// import 'object.js';
+// console.log(loadObject);
+
+
+var characters = [
+    {
+        name: 'Ant-Man',
+        imageUrl: "https://thesource.com/wp-content/uploads/2015/07/ant-man1.jpg",
+    }
+    // {
+    //     name: 'Black Panther',
+    //     imageUrl: "https://cdn.vox-cdn.com/thumbor/SQnBpaoYz4vaIZ71gY_Oho2izjI=/0x0:1056x439/1200x800/filters:focal(495x39:663x207)/cdn.vox-cdn.com/uploads/chorus_image/image/58986389/Screen_Shot_2018_01_11_at_3.47.24_PM.0.png",
+    // },
+    // {
+    //     name: 'Black Widow',
+    //     imageUrl: "https://am22.akamaized.net/tms/cnt/uploads/2018/05/black-widow-scarlett-johansson-marvel-the-avengers-1200x752.jpg",
+    // },
+    // {
+    //     name: 'Bucky',
+    //     imageUrl: "https://static.tvtropes.org/pmwiki/pub/images/mcu_bucky_40s.png",
+    // },
+    // {
+    //     name: 'Captain America',
+    //     imageUrl: "https://onthegoinmco.com/wp-content/uploads/2013/11/Cap.jpg",
+    // },
+    // {
+    //     name: 'Captain Marvel',
+    //     imageUrl: "https://static.independent.co.uk/s3fs-public/styles/story_large/public/thumbnails/image/2018/06/15/15/brie-larson-captain-marvel-0.jpg",
+    // },
+    // {
+    //     name: 'Deadpool',
+    //     imageUrl: "https://i2-prod.mirror.co.uk/incoming/article6809012.ece/ALTERNATES/s615b/Ryan-Reynolds.jpg",
+    // }
+]
+
+var reader;
 // function jupload()
 // {
 //     $(".imageholder").append('<img src="./images/loading.gif">');
 // }
-
-function juploadstop(result)
-{
-    if(result==0)
-    {
+function juploadstop(result) {
+    if (result == 0) {
         $(".imageholder").html("");
 
     }
     // the result will be the path to the image
-    else if(result!=0)
-    {
+    else if (result != 0) {
         $(".imageholder").html("");
         // imageplace is the class of the div where you want to add the image  
-        $(".imageplace").append("<img src='"+result+"'>");
-    }   
+        $(".imageplace").append("<img src='" + result + "'>");
+    }
 }
-
-$('#submit_form').on("click", function(){
-    $.ajax({
-      type: 'POST',
-      url: path/fileUpload.php,
-      data: image_input_name
-    });
-  
-  //after submitting, get the url of the image form the server
-  
-    $('#div_to_display_image').html("<img src='path/image_file.jpg' alt='this' />");
-  
-  });
-
 
 // function lastResult (heroName) {
 
 // $('.list-group').append("<li class='list-group-item'>"+"You could be related to " + heroname +" </li>")
 // }
-
-
 var config = {
     apiKey: "AIzaSyBMxbRavlaUZpJQWiMiCoFuT1hT_un6iQ0",
     authDomain: "ballmer-peak-project1.firebaseapp.com",
@@ -46,63 +62,96 @@ var config = {
     projectId: "ballmer-peak-project1",
     storageBucket: "",
     messagingSenderId: "520183238866"
-  };
-  
-  firebase.initializeApp(config);
-  
-  var database = firebase.database();
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
 
 
 
-$(document).ready(function(){
-    $("#collapseExample").click(function() {
-        $('.list-group').append("<li class='list-group-item'>"+"You could be related to " + heroname +" </li>")
+$(document).ready(function () {
+    $("#collapseExample").click(function () {
+        $('.list-group').append("<li class='list-group-item'>" + "You could be related to " + heroname + " </li>")
     });
 });
 
 var compareFace = function () {
-
-    var name = $(this).attr("data-name");
-    var queryURL = "https://api-us.faceplusplus.com/facepp/v3/search" +
-      name + "AuLF26cH7L5MFtwVuC9hhqCQbiB6K8HR";
-
-    $.ajax({
-      url: queryURL,
-      method: "POST"
-    })
-      .then(function (response) {
-        var results = response.data;
-        for (var i = 0; i < results.length; i++) {
-
-        //   var gifDiv = $("<div class='item'>");
-        //   gifDiv.addClass("gifDiv");
-        //   // pulling rating of gif
-        //   // var rating = results[i].rating;
-        //   var rating = $("<p>").text("Rating: " + results[i].rating);
-        //   gifDiv.append(rating);
-        //   // pulling gif
-        //   var nameImage = $("<img>");
-        //   nameImage.attr("src", results[i].images.fixed_height_small_still.url);
-        //   nameImage.attr("data-still", results[i].images.fixed_height_small_still.url);
-        //   nameImage.attr("data-animate", results[i].images.fixed_height_small.url);
-        //   nameImage.attr("data-state", "still");
-        //   // nameImage.attr("onclick", playPause)
-        //   nameImage.addClass("image");
-        //   gifDiv.append(nameImage);
-
-          $("#marvel-anc").append(gifDiv);
+    console.log("C", characters)
+    for (var i = 0; i < characters.length; i++) {
+        console.log("In for loop");
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://api-us.faceplusplus.com/facepp/v3/compare?api_key=AuLF26cH7L5MFtwVuC9hhqCQbiB6K8HR&api_secret=ovhD7RlOb-4xLChOM2EGP0sKaCnZ9aFV&image_file1=" + characters[i + 1] + "&image_url2=" + characters[i].imageUrl,
+            "method": "POST",
+            // "headers": {
+            //     "cache-control": "no-cache",
+            //     "Access-Control-Allow-Origin" : "*",
+            //     "Access-Control-Allow-Methods": "POST,GET,PUT,DELETE",
+            //     "Access-Control-Allow-Headers": "Authorization, Lang",
+            //     "postman-token": "1bc6e57f-f4e9-e6fa-478c-fed70312feef"
         }
-      });
-  }
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    // let postJson = {
+    //     "api_key": "AuLF26cH7L5MFtwVuC9hhqCQbiB6K8HR",
+    //     "api_secret": "ovhD7RlOb-4xLChOM2EGP0sKaCnZ9aFV",
+    //     "image_url1": characters[i+1].imageUrl,
+    //     "image_url2": characters[i].imageUrl
+    // }
+    // headers = {
+    //     "Access-Control-Allow-Origin" : "*"
+    // }
+    // console.log(reader)
+    // var queryURL = "https://api-us.faceplusplus.com/facepp/v3/compare";
+
+    // $.ajax({
+    //     url: queryURL,
+    //     headers: headers,
+    //     method: "POST",
+    //     dataType: 'jsonp',
+    //     body: JSON.parse(JSON.stringify(postJson))
+    // }).then(function (error, response, body) {
+    // console.log(error)
+    // console.log(response)
+    // console.log(body)
+    // $("#marvel-anc").append(gifDiv);
+    //     })
+}
+// }
+
 
 //   displaying the uploaded image
-$('#submit').click(function(){
-    var photo = $('#input').val();
-    $('#prof-pic').attr('<img src=' + photo + '>')
-  });
+
+// $('#btn').on('click', function () {           //on click of button
+//     $('#prof-pic').attr('src', $('#url').val());  //get the input value and 
+//     console.log('#url');
+//     compareFace()
+
+// })
+$(function () {
+    $(":file").change(function () {
+        if (this.files && this.files[0]) {
+            console.log(this.files)
+            reader = new FileReader();
+            reader.onload = imageIsLoaded;
+            reader.readAsDataURL(this.files[0]);
+            compareFace()
+        }
+    });
+});
+
+function imageIsLoaded(e) {
+    $('#prof-pic').attr('src', e.target.result);
+};
+
+
 // comparing the uploaded image with array of heroes
 
 // displaying details about the marvel hero
 
 // pushing past results from Firebase to the table
-
---
+++ assets/javascript/object.js
@@ -0,0 +1,118 @@
+export function loadObject() {
+    return [
+        {
+            name: 'Ant Man',
+            imageUrl: "http://thesource.com/wp-content/uploads/2015/07/ant-man1.jpg",
+        },
+        {
+            name: 'Black Panther',
+            imageUrl: "https://cdn.vox-cdn.com/thumbor/SQnBpaoYz4vaIZ71gY_Oho2izjI=/0x0:1056x439/1200x800/filters:focal(495x39:663x207)/cdn.vox-cdn.com/uploads/chorus_image/image/58986389/Screen_Shot_2018_01_11_at_3.47.24_PM.0.png",
+        },
+        {
+            name: 'Black Widow',
+            imageUrl: "https://am22.akamaized.net/tms/cnt/uploads/2018/05/black-widow-scarlett-johansson-marvel-the-avengers-1200x752.jpg",
+        },
+        {
+            name: 'Bucky',
+            imageUrl: "https://static.tvtropes.org/pmwiki/pub/images/mcu_bucky_40s.png",
+        },
+        {
+            name: 'Captain America',
+            imageUrl: "https://onthegoinmco.com/wp-content/uploads/2013/11/Cap.jpg",
+        },
+        {
+            name: 'Captain Marvel',
+            imageUrl: "https://static.independent.co.uk/s3fs-public/styles/story_large/public/thumbnails/image/2018/06/15/15/brie-larson-captain-marvel-0.jpg",
+        },
+        {
+            name: 'Deadpool',
+            imageUrl: "https://i2-prod.mirror.co.uk/incoming/article6809012.ece/ALTERNATES/s615b/Ryan-Reynolds.jpg",
+        },
+        // {
+        //     name: 'Hank Pym',
+        //     imageUrl: "assets/images/Aqua.png",
+        // },
+        // {
+        //     name: 'Hawkeye',
+        //     imageUrl: "assets/images/Flash.png",
+        // },
+        // {
+        //     name: 'Hela',
+        //     imageUrl: "assets/images/Aqua.png",
+        // },
+        // {
+        //     name: 'The Hulk',
+        //     imageUrl: "assets/images/Flash.png",
+        // },
+        // {
+        //     name: 'Iron Man',
+        //     imageUrl: "assets/images/Aqua.png",
+        // },
+        // {
+        //     name: 'Iron Monger',
+        //     imageUrl: "assets/images/Flash.png",
+        // },
+        // {
+        //     name: 'Jessica Jones',
+        //     imageUrl: "assets/images/Aqua.png",
+        // },
+        // {
+        //     name: 'Loki',
+        //     imageUrl: "assets/images/Flash.png",
+        // },
+        // {
+        //     name: 'Nakia',
+        //     imageUrl: "assets/images/Aqua.png",
+        // },
+        // {
+        //     name: 'Nick Fury',
+        //     imageUrl: "assets/images/Flash.png",
+        // },
+        // {
+        //     name: 'Professor X',
+        //     imageUrl: "assets/images/Aqua.png",
+        // },
+        // {
+        //     name: 'Quick Silver',
+        //     imageUrl: "assets/images/Flash.png",
+        // },
+        // {
+        //     name: 'War Machine',
+        //     imageUrl: "assets/images/Aqua.png",
+        // },
+        // {
+        //     name: 'Scarlet Witch',
+        //     imageUrl: "assets/images/Flash.png",
+        // },
+        // {
+        //     name: 'Spider-Man',
+        //     imageUrl: "assets/images/Aqua.png",
+        // },
+        // {
+        //     name: 'Star Lord',
+        //     imageUrl: "assets/images/Flash.png",
+        // },
+        // {
+        //     name: 'Bucky',
+        //     imageUrl: "assets/images/Aqua.png",
+        // },
+        // {
+        //     name: 'The Wasp',
+        //     imageUrl: "assets/images/Flash.png",
+        // },
+        // {
+        //     name: 'Thor',
+        //     imageUrl: "assets/images/Aqua.png",
+        // },
+        // {
+        //     name: 'Valkyrie',
+        //     imageUrl: "assets/images/Aqua.png",
+        // },
+        // {
+        //     name: 'Wolverine',
+        //     imageUrl: "assets/images/Aqua.png",
+        // },
+    ]
+}
+
+
